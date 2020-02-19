32e47f60c260b1dcc5a0bc86dbf180b3e712b845
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-19 17:24:08 +0530

    Added: Loose or win dolar one from stake.

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -2,4 +2,15 @@
 echo Welcome to Gambling game.
 
 stake=100
+money=$stake
 bet=1
+random=$((RANDOM%2))
+if (( random==bet ))
+then
+	echo win
+	((money++))
+else
+	echo loose
+	((money--))
+fi
+echo $money
