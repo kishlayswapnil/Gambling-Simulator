72878474cd0a0c0e75729219f7ba0f140c373289
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-20 17:56:56 +0530

    Added: Luckies and Unluckiest day has been added.

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -5,6 +5,7 @@ echo Welcome to Gambling game.
 STAKE=100
 BET=1
 WIN=1
+declare -A Day
 #Logic for days and win loss check.
 for(( day=1; day<=20; day++ ))
 do
@@ -17,17 +18,28 @@ do
 		random=$((RANDOM%2))
 		if (( random==WIN ))
 		then
-			echo win
 			((money++))
 		else
-			echo loss
 			((money--))
 		fi
 		total=$(($money-$STAKE))
 	done
+	totalMoney=$(($totalMoney+$total))
 	totalPossibility[day]=$money
 	dayWinLoose[day]=$total
+	Day[Day$day]=$totalMoney
 done
 #Displaying results.
 echo "Total possibilities of win and loss is -: "${totalPossibility[@]}
 echo "Days won and Loss by -: "${dayWinLoose[@]}
+
+#Function to sort the dictionary Day has been added.
+function sorting() {
+	for key in ${!Day[@]}
+	do
+		echo  $key '-:' ${Day[$key]}
+	done | sort -rn -k3
+}
+echo "$(sorting)"
+echo "Luckiest Day is:-  $(sorting | head -1)"
+echo "Unluckiest Day is:-  $(sorting | tail -1)"
