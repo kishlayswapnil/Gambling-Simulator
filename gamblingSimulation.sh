2c1dbf36ad519daf133edb8d5d83a84431cef6ba
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-21 10:17:50 +0530

    Added: The If statement to check the condition if total money is more than stake.

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,37 +1,52 @@
 #!/bin/bash -x
 echo Welcome to Gambling game.
 
+function gambling() {
 #Constatns declared
-STAKE=100
-BET=1
-WIN=1
-declare -A Day
+	STAKE=100
+	BET=1
+	WIN=1
+
+	declare -A Day
 #Logic for days and win loss check.
-for(( day=1; day<=20; day++ ))
-do
-	#Varriables declared.
-	money=$STAKE
-	minimumStake=$(($STAKE-$STAKE/2))
-	maximumStake=$(($STAKE+$STAKE/2))
-	while (( $money != $minimumStake && $money != $maximumStake ))
+	for(( day=1; day<=20; day++ ))
 	do
-		random=$((RANDOM%2))
-		if (( random==WIN ))
-		then
-			((money++))
-		else
-			((money--))
-		fi
-		total=$(($money-$STAKE))
+#Varriables declared.
+		money=$STAKE
+		minimumStake=$(($STAKE-$STAKE/2))
+		maximumStake=$(($STAKE+$STAKE/2))
+		while (( $money != $minimumStake && $money != $maximumStake ))
+		do
+			random=$((RANDOM%2))
+			if (( random==WIN ))
+			then
+				((money++))
+			else
+				((money--))
+			fi
+			total=$(($money-$STAKE))
+		done
+		totalMoney=$(($totalMoney+$total))
+		totalPossibility[day]=$money
+		dayWinLoose[day]=$total
+		Day[Day$day]=$totalMoney
 	done
-	totalMoney=$(($totalMoney+$total))
-	totalPossibility[day]=$money
-	dayWinLoose[day]=$total
-	Day[Day$day]=$totalMoney
-done
 #Displaying results.
-echo "Total possibilities of win and loss is -: "${totalPossibility[@]}
-echo "Days won and Loss by -: "${dayWinLoose[@]}
+	echo "Total possibilities of win and loss is -: "${totalPossibility[@]}
+	echo "Days won and Loss by -: "${dayWinLoose[@]}
+	echo "$(sorting)"
+	echo "Luckiest Day is:-  $(sorting | head -1)"
+	echo "Unluckiest Day is:-  $(sorting | tail -1)"
+	echo "Total money left -: "$totalMoney
+#Condition checking to repeat the game if in profit.
+	if (( $STAKE<$totalMoney ))
+	then
+		echo "You can continue betting."
+		gambling
+	else
+		echo "Sorry to say sir but you have nothing to loose now."
+	fi
+}
 
 #Function to sort the dictionary Day has been added.
 function sorting() {
@@ -40,6 +55,5 @@ function sorting() {
 		echo  $key '-:' ${Day[$key]}
 	done | sort -rn -k3
 }
-echo "$(sorting)"
-echo "Luckiest Day is:-  $(sorting | head -1)"
-echo "Unluckiest Day is:-  $(sorting | tail -1)"
+#Calling the function.
+gambling
